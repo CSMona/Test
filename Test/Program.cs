ad50237edb67354e10f10ac46a95f80db304f569
Mona Abdelmnem <monasallam400@gmail.com>
2022-07-11 17:32:22 +0200

    Initial commit

---
+++ Test/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            string[] name = { "mona", "Ali", "sara","Noor" };
+
+            IEnumerable<string> names = name
+                                    .Where(n => n.Contains("a"))
+                                    .OrderBy(n => n.Length)
+                                    .Select(n => n.ToUpper());
+            foreach (var item in names)
+            {
+                Console.WriteLine(item);
+            }
+            
+
+            //var regiterUser = new RegisterUser();
+            //var emailVerification = new EmailVerification();
+            //var sMSVerivication = new SMSVerivication();
+            //regiterUser.UserRegisterEvent += emailVerification.OnUserRegister;
+            //regiterUser.UserRegisterEvent += sMSVerivication.OnUserRegister;
+            //regiterUser.RegisterMethod();
+
+            //**************Deleget****************
+            //var regiterUser = new RegisterUser();
+            //var emailVerification = new EmailVerification();
+            //var sMSVerivication = new SMSVerivication();
+            //regiterUser.UserRegisterDelegetInstance += emailVerification.OnUserRegister;
+            //regiterUser.UserRegisterDelegetInstance += sMSVerivication.OnUserRegister;
+            //regiterUser.RegisterMethod();
+
+
+
+        }
+    }
+    public class RegisterUser
+    {
+        public delegate void UserRegisterDeleget(object o, EventArgs eventArgs);//declare deleget
+        public event UserRegisterDeleget UserRegisterEvent;// declare event
+
+        public void RegisterMethod()
+        {
+            Console.WriteLine("User Register");
+            if (UserRegisterEvent != null)
+            {
+                UserRegisterEvent(this, EventArgs.Empty);//call event
+            }
+        }
+
+    }
+
+    public class EmailVerification
+    {
+        public void OnUserRegister(object o, EventArgs eventArgs)
+        {
+            Console.WriteLine("Email Sent");
+        }
+    }
+
+    public class SMSVerivication
+    {
+        public void OnUserRegister(object o, EventArgs eventArgs)
+        {
+            Console.WriteLine("SMS Sent");
+        }
+    }
+
+    //***************************Deleget****************************
+
+    //public class RegisterUser
+    //{
+    //   public delegate void UserRegisterDeleget();//declare ddelger
+    //   public UserRegisterDeleget UserRegisterDelegetInstance;// instance delecget
+
+    //    public void RegisterMethod()
+    //    {
+    //        Console.WriteLine("User Register");
+    //        if(UserRegisterDelegetInstance!=null)
+    //        {
+    //            UserRegisterDelegetInstance();//call delegete
+    //        }
+    //    }
+
+    //}
+
+    //public class EmailVerification
+    //{
+    //    public void OnUserRegister()
+    //    {
+    //        Console.WriteLine("Email Sent");
+    //    }
+    //}
+
+    //public class SMSVerivication
+    //{
+    //    public void OnUserRegister()
+    //    {
+    //        Console.WriteLine("SMS Sent");
+    //    }
+    //}
+}
---
+++ Test/Stock.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+  delegate void ChangepriceHandler(decimal oldprice, decimal newprice);
+   public class Stock
+    {
+        string symbole;
+        decimal price;
+
+        public Stock(string symbole) => this.symbole = symbole;
+        event ChangepriceHandler ChangePrice;
+
+        public decimal Price
+        {
+            get => price;
+            set
+            {
+                if (price == value) return;
+                decimal OldVal = price;
+                price = value;
+                if (ChangePrice != null)
+                    ChangePrice(OldVal, price);
+            }
+        }
+
+
+    }
+}
