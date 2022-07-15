4f1486c85588bcc531fb133d4276d04bbf4ea884
Mona Abdelmnem <monasallam400@gmail.com>
2022-07-15 20:03:17 +0200

    C Sharp

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,16 +1,84 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Test
+ namespace Test
 {
-    class Program
+   public class Program
     {
+
+
+        public delegate void mydelget();
+        class Test {
+
+
+
+            public mydelget mydel;
+            public int? x;
+            public string name;
+            public Test()
+            {
+                mydel = myfunc;
+            }
+            public void myfunc()
+            {
+                Console.WriteLine("myfunc");
+            }
+
+            public void myfunc2()
+            {
+                Console.WriteLine("myfunc2");
+            }
+
+  
+
+
+    }
+
 
         static void Main(string[] args)
         {
+        
+
+
+
+	{
+
+	}
+
+            //MemberInfo[] memberInfos = type.GetMembers();
+            //MethodInfo[] methodInfos = type.GetMethods();
+            //ConstructorInfo[] constructorInfos = type.GetConstructors();
+            //foreach (var item in propertyInfos)
+            //{
+            //    Console.WriteLine(item.Name);
+            //}
+
+            //test.mydel();
+            //test.mydel =test.myfunc2;
+            //test.mydel();
+            FontStyle c = FontStyle.Bold ^ FontStyle.ABC^ FontStyle.ABC;
+            FontStyle c2 = c ^ FontStyle.Bold;
+            //Console.WriteLine(c);
+
+        }
+        [Flags]
+          public  enum FontStyle
+        {
+            Bold = 1,
+            Italic = 1 << 1,
+            Underline=1<<2,
+            ABC=1<<3
+        
+
+    } 
+    }
+
+
+
             //using (var enumerator = "beer".GetEnumerator())
             //{
             //    while (enumerator.MoveNext())
@@ -20,43 +88,43 @@ namespace Test
             //    }
             //}
 
-            int? x = null;
-            int y = x ?? 5; // y is 5
-            int? a = null, b = 1, c = 2;
-            Console.WriteLine(a ?? b ?? c); // 1 (first non-null value)
+            //int? x = null;
+            //int y = x ?? 5; // y is 5
+            //int? a = null, b = 1, c = 2;
+            //Console.WriteLine(a ?? b ?? c); // 1 (first non-null value)
 
-            IEnumerable<int>facts(int factcont)
-            {
-                int prev = 1;
-                int curr = 1;
-                for (int i = 0; i < factcont; i++)
-                {
-                   
-                    yield return prev;
-                    
-                    int newCount = prev + curr;
-                    prev = curr;
-                    curr = newCount;
-                    
+            //IEnumerable<int>facts(int factcont)
+            //{
+            //    int prev = 1;
+            //    int curr = 1;
+            //    for (int i = 0; i < factcont; i++)
+            //    {
 
-                }
-               
-            }
+            //        yield return prev;
 
-            IEnumerable<int> Evenet(IEnumerable<int>Sequenceeven)
-            {
-                foreach (var item in Sequenceeven)
-                {
-                    if (item % 2 == 0)
-                        yield return item;
-                }
-             
-            }
+            //        int newCount = prev + curr;
+            //        prev = curr;
+            //        curr = newCount;
 
-            foreach (var item in Evenet(facts(6)) )
-            {
-                Console.Write(  item+" ");
-            }
+
+            //    }
+
+            //}
+
+            //IEnumerable<int> Evenet(IEnumerable<int>Sequenceeven)
+            //{
+            //    foreach (var item in Sequenceeven)
+            //    {
+            //        if (item % 2 == 0)
+            //            yield return item;
+            //    }
+
+            //}
+
+            //foreach (var item in Evenet(facts(6)) )
+            //{
+            //    Console.Write(  item+" ");
+            //}
 
 
             //           string[] names = { "Tom", "Dick", "Harry", "Mary", "Jay" };
@@ -125,8 +193,7 @@ namespace Test
 
 
 
-    }
-    }
+   
     public class RegisterUser
     {
         public delegate void UserRegisterDeleget(object o, EventArgs eventArgs);//declare deleget
